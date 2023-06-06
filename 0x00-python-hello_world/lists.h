58b3bb13c6ed14752f73d89f835f488469dcf897
salmaelmaimouni <salmaelmaimouni20@gmail.com>
2023-06-06 07:43:47 -0700

    reademe

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -11,16 +11,15 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head,const int n);
-void free_listint(listint *head);
-int chek_cycle(listint_t *list);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-#enddif /* LISTS_H */
+#endif /* LISTS_H */
