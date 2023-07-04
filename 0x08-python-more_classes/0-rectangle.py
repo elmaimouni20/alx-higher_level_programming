cd070d76686c4cc7350a8a18a8f985d72379f28b
salmaelmaimouni <salmaelmaimouni20@gmail.com>
2023-07-03 18:42:41 -0700

    all task done

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
